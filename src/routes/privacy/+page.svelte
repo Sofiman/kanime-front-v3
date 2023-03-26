62d166f84729bd09b20b864641e5541f4560913b
Sofiane <sofiane.meftah@outlook.fr>
2023-03-26 13:17:36 +0100

    feat: Add privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,100 @@
+<div class="body">
+    <div class="policy">
+        <h1>Privacy Policy</h1>
+
+        <p>At <span>Kanime</span>, accessible from <a href="/">https://kanime.fr</a>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Kanime and how we use it.</p>
+
+        <p>If you have additional questions or require more information about our
+            Privacy Policy, do not hesitate to contact us at <a href="mailto://contact@kanime.fr">contact@kanime.fr</a></p>
+
+        <h2>General Data Protection Regulation (GDPR)</h2>
+        <p>We are a Data Controller of your information.</p>
+
+        <p><span>Kanime</span> legal basis for collecting and using the personal information described in this Privacy Policy depends on the Personal Information we collect and the specific context in which we collect the information:</p>
+        <ul>
+            <li>Kanime needs to perform a contract with you</li>
+            <li>You have given Kanime permission to do so</li>
+            <li>Processing your personal information is in Kanime legitimate interests</li>
+            <li>Kanime needs to comply with the law</li>
+        </ul>
+        <br />
+
+        <p><span>Kanime</span> will retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.</p>
+
+        <p>If you are a resident of the European Economic Area (EEA), you have certain data protection rights. If you wish to be informed what Personal Information we hold about you and if you want it to be removed from our systems, please contact us.</p>
+        <p>In certain circumstances, you have the following data protection rights:</p>
+        <ul>
+            <li>The right to access, update or to delete the information we have on you.</li>
+            <li>The right of rectification.</li> 
+            <li>The right to object.</li>
+            <li>The right of restriction.</li>
+            <li>The right to data portability</li>
+            <li>The right to withdraw consent</li>
+        </ul>
+
+        <h2>Log Files</h2>
+
+        <p><span>Kanime</span> follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include <span>internet protocol (IP) addresses</span>, <span>date and time stamp</span> and <span>referring/exit pages</span>. These are <b>not</b> linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+
+        <h2>Cookies and Web Beacons</h2>
+
+        <p><span>Kanime</span> does <b>not</b> collect any Personal Identifiable Information from users that did or did not create any account except the email address used to login to their account.</p>
+
+        <p>Like any other website, <span>Kanime</span> uses cookies. A single cookie is used to store information of the current user session such that the user can stay logged in when they visit <span>Kanime</span> again. </p>
+
+        <p>Other cookies may be used by <a href="https://www.cloudflare.com/" target="_blank" rel="noreferrer">Cloudflare</a> in order to protect <span>Kanime</span> against organized attacks. Note that <span>Kanime</span> has no access to or control over these cookies.</p>
+
+        <h2>Third Party Privacy Policies</h2>
+
+        <p><span>Kanime's Privacy Policy</span> does not apply to other websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party websites for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+
+        <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
+
+        <h2>Children's Information</h2>
+
+        <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+
+        <p><span>Kanime</span> does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+
+        <h2>Online Privacy Policy Only</h2>
+
+        <p>Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in <span>Kanime</span>. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+
+        <h2>Consent</h2>
+
+        <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+    </div>
+</div>
+<style lang="scss">
+    .body {
+        display: flex;
+        justify-content: center;
+        margin: 16px;
+
+        .policy {
+            max-width: 50vw;
+
+            span {
+                color: #F18FF3;
+            }
+
+            a {
+                color: #A3A3B0;
+            }
+
+            ul li {
+                margin-left: 32px;
+            }
+
+            h1 {
+                margin: 32px 0;
+            }
+        }
+    }
+
+    @media screen and (max-width: 800px) {
+        .body .policy {
+            max-width: none;
+        }
+    }
+</style>
